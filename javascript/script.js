fd6f329a82fb724ed4afe82c896214df837c950d
Shirley Wong <shirleywsywong@gmail.com>
2019-08-27 18:38:33 -0400

    CSS: added styles, JS: API function and filtering

--- javascript/script.js
+++ javascript/script.js
@@ -1,22 +1,35 @@
+const travelApp = {};
+travelApp.baseUrl = `https://restcountries.eu/rest/v2/all`;
 
-
-const artApp = {};
-artApp.baseUrl = `https://restcountries.eu/rest/v2/all`;
-
-artApp.getArt = function() {
+travelApp.popFilteredArray = [];
+travelApp.getCountries = function() {
     $.ajax({
-        url: artApp.baseUrl,
+        url: travelApp.baseUrl,
         method: 'GET',
         datatype: 'json',
     }).then(function(result) {
-        console.log(result);
+        for(let i = 1; i <= 249; i++) {
+            if (result[i].population > 15000000 && result[i].flag != "") {
+              travelApp.popFilteredArray.push(result[i])
+            }
+          }
+          console.log(travelApp.popFilteredArray)
+        // name, capital, flag, languages, currencies,Demonim,region, lating(strechgoal), timezone(optional)
+        // console.log(result);
+        // console.log(result[0].name);
+        // console.log(result[0].capital);
+        // console.log(result[0].flag);
+        // console.log(result[0].languages);
+        // console.log(result[0].currencies);
     }) 
 }
 
-artApp.init = function () {
-    artApp.getArt();
+
+//capital, flag, name, languages object, currency
+travelApp.init = function () {
+    travelApp.getCountries();
 }
 
 $(document).ready(function () {
-    artApp.init();
+    travelApp.init();
 })
